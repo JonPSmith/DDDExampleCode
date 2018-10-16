6d877980b9924a8eeba87d765834fd924a5398d1
Jon Smith <jon.smith@selectiveanalytics.com>
2018-10-16 10:21:27 +0100

    Added POCO repository

---
+++ PocoRepository/Repository.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2018 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.Linq;
+using DataLayer.EfCode;
+using Microsoft.EntityFrameworkCore;
+
+namespace PocoRepository
+{
+    public class Repository
+    {
+        private readonly PocoOnlyDbContext _context;
+
+        public Repository(PocoOnlyDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddReview(int id, int numStars, string comment, string voterName)
+        {
+            var book = _context.Books.Include(x => x.Reviews)
+                .SingleOrDefault(x => x.Id == id);
+            if (book == null)
+                return false;
+
+            book.AddReview(numStars, comment, voterName);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ Test/UnitTests/TestPocoOnly/TestPocoRepository.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) 2018 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataLayer.EfClasses.PocoOnly;
+using DataLayer.EfClasses.PocoOnly.Support;
+using DataLayer.EfCode;
+using Microsoft.EntityFrameworkCore;
+using PocoRepository;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test.UnitTests.TestPocoOnly
+{
+    public class TestPocoRepository
+    {
+
+
+        [Fact]
+        public void TestAddReviewToBookOk()
+        {
+            //SETUP1
+            var options = SqliteInMemory.CreateOptions<PocoOnlyDbContext>();
+            using (var context = new PocoOnlyDbContext(options))
+            {
+                context.Database.EnsureCreated();
+                var book = new Book("test", null, DateTime.UtcNow, "Me", 123, null,
+                    new List<Author> {new Author("Me", "me@me.com")});
+                context.Add(book);
+                context.SaveChanges();
+            }
+            using (var context = new PocoOnlyDbContext(options))
+            {
+                var rep = new Repository(context);
+
+                //ATTEMPT
+                var ok = rep.AddReview(1, 2, "OK", "Me");
+
+                //VERIFY
+                ok.ShouldBeTrue();
+            }
+            using (var context = new PocoOnlyDbContext(options))
+            {
+                context.Set<Review>().Count().ShouldEqual(1);
+                context.Books.Include(x => x.Reviews)
+                    .Single().Reviews.SingleOrDefault().VoterName.ShouldEqual("Me");
+            }
+        }
+
+
+
+    }
+}
